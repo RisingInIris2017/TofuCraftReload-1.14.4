884f8f69fd2a7f0579808ed13c2b3cff1e55e9ae
pentantan <shutux500@gmail.com>
2019-11-27 08:20:55 +0900

    tweek go to bed ai

--- src/main/java/baguchan/mcmod/tofucraft/entity/ai/GoToBedGoal.java
+++ src/main/java/baguchan/mcmod/tofucraft/entity/ai/GoToBedGoal.java
@@ -23,7 +23,7 @@ public class GoToBedGoal extends MoveToBlockGoal {
      * Returns whether the EntityAIBase should begin execution.
      */
     public boolean shouldExecute() {
-        return !this.creature.isBeingRidden() && !this.creature.world.isDaytime() && this.creature.getAttackTarget() == null && super.shouldExecute();
+        return !this.creature.isBeingRidden() && this.creature.world.getDimension().isSurfaceWorld() && !this.creature.world.isDaytime() && this.creature.getAttackTarget() == null && this.searchForDestination();
     }
 
     @Override
