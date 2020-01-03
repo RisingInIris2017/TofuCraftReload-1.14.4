fdb1498c6cf94b93a1007888ba24c84573d68c9e
pentantan <shutux500@gmail.com>
2020-01-03 22:40:32 +0900

    piece name changed

--- src/main/java/baguchan/mcmod/tofucraft/world/gen/feature/structure/ITofuStructurePieceType.java
+++ src/main/java/baguchan/mcmod/tofucraft/world/gen/feature/structure/ITofuStructurePieceType.java
@@ -1,13 +1,14 @@
 package baguchan.mcmod.tofucraft.world.gen.feature.structure;
 
+import baguchan.mcmod.tofucraft.TofuCraftCore;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.gen.feature.structure.IStructurePieceType;
 
 import java.util.Locale;
 
 public interface ITofuStructurePieceType {
-    IStructurePieceType TOFUVI_PIECE = register(TofuVillageStructure.TofuVillage::new, "TofuVi");
-    IStructurePieceType TOFUCASTLE_PIECE = register(TofuCastlePieces.Piece::new, "TofuCa");
+    IStructurePieceType TOFUVI_PIECE = register(TofuVillageStructure.TofuVillage::new, TofuCraftCore.MODID + ".tofuvillage");
+    IStructurePieceType TOFUCASTLE_PIECE = register(TofuCastlePieces.Piece::new, TofuCraftCore.MODID + ".tofucastle");
 
 
     static IStructurePieceType register(IStructurePieceType type, String key) {
