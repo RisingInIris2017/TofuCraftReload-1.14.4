308f687caf8207e72481c61089d3fb9ba560621e
pentantan <shutux500@gmail.com>
2020-05-12 23:45:27 +0900

    clean code

--- src/main/java/baguchan/mcmod/tofucraft/block/TofuMushroomBlock.java
+++ src/main/java/baguchan/mcmod/tofucraft/block/TofuMushroomBlock.java
@@ -35,8 +35,6 @@ public class TofuMushroomBlock extends BushBlock implements IGrowable {
     }
 
     public void tick(BlockState state, ServerWorld worldIn, BlockPos pos, Random rand) {
-
-
         if (rand.nextInt(30) == 0 && worldIn.canSeeSky(pos)) {
             if (func_226940_a_(worldIn, pos, state, rand)) {
                 addNewMushroom(state, worldIn, pos, rand);
