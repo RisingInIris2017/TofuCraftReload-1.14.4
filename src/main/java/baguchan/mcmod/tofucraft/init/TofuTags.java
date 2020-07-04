ffd14879821afbb48a7e1409822ba8084ce37cfe
pentantan <shutux500@gmail.com>
2020-07-04 13:34:42 +0900

    1.16.1-0.0.1.1 fixed crash for tags misspell

--- src/main/java/baguchan/mcmod/tofucraft/init/TofuTags.java
+++ src/main/java/baguchan/mcmod/tofucraft/init/TofuTags.java
@@ -11,20 +11,30 @@ import net.minecraft.tags.ItemTags;
 public class TofuTags {
     public static class Fluids {
 
-        public static final ITag.INamedTag<Fluid> SOYMILK = FluidTags.makeWrapperTag("soymilk");
+        public static final ITag.INamedTag<Fluid> SOYMILK = makeWrapperTag("soymilk");
 
+        public static ITag.INamedTag<Fluid> makeWrapperTag(String id) {
+            return FluidTags.makeWrapperTag("tofucraft:" + id);
+        }
     }
 
     public static class Items {
 
-        public static final ITag.INamedTag<Item> TOFU = ItemTags.makeWrapperTag("tofu");
+        public static final ITag.INamedTag<Item> TOFU = makeWrapperTag("tofu");
 
+        public static ITag.INamedTag<Item> makeWrapperTag(String id) {
+            return ItemTags.makeWrapperTag("tofucraft:" + id);
+        }
     }
 
     public static class Blocks {
 
-        public static final ITag.INamedTag<Block> TOFULOG = BlockTags.makeWrapperTag("tofulogs");
-        public static final ITag.INamedTag<Block> TOFUTERRAIN = BlockTags.makeWrapperTag("tofu_terrain");
+        public static final ITag.INamedTag<Block> TOFULOG = makeWrapperTag("tofulogs");
+        public static final ITag.INamedTag<Block> TOFUTERRAIN = makeWrapperTag("tofu_terrain");
 
+
+        public static ITag.INamedTag<Block> makeWrapperTag(String id) {
+            return BlockTags.makeWrapperTag("tofucraft:" + id);
+        }
     }
 }
