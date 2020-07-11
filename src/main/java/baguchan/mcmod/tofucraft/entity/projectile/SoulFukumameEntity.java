28c23049db133ad392f0e4f342cdbeaf780135fb
pentantan <shutux500@gmail.com>
2020-07-11 17:36:28 +0900

    fixed particle

--- src/main/java/baguchan/mcmod/tofucraft/entity/projectile/SoulFukumameEntity.java
+++ src/main/java/baguchan/mcmod/tofucraft/entity/projectile/SoulFukumameEntity.java
@@ -50,7 +50,7 @@ public class SoulFukumameEntity extends ThrowableEntity {
             double d0 = 0.08D;
 
             for (int i = 0; i < 6; ++i) {
-                this.world.addParticle(new ItemParticleData(ParticleTypes.ITEM, new ItemStack(TofuItems.SEEDS_SOYBEAN_NETHER)), this.getPosX(), this.getPosY(), this.getPosZ(), ((double) this.rand.nextFloat() - 0.5D) * 0.08D, ((double) this.rand.nextFloat() - 0.5D) * 0.08D, ((double) this.rand.nextFloat() - 0.5D) * 0.08D);
+                this.world.addParticle(new ItemParticleData(ParticleTypes.ITEM, new ItemStack(TofuItems.SEEDS_SOYBEAN_SOUL)), this.getPosX(), this.getPosY(), this.getPosZ(), ((double) this.rand.nextFloat() - 0.5D) * 0.08D, ((double) this.rand.nextFloat() - 0.5D) * 0.08D, ((double) this.rand.nextFloat() - 0.5D) * 0.08D);
             }
         }
     }
